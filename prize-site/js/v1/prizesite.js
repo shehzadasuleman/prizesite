f39e17633365715712045d3fd01a3d8df7a58da8
rohaanali <rohaan.ali15@gmail.com>
2018-12-19 14:20:38 +0500

    Add scrolling to textbox for mobile phones

---
+++ prize-site/js/v1/prizesite.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+
+    $(window).on("resize", function (e) {
+        checkScreenSize();
+    });
+
+    checkScreenSize();
+
+    function checkScreenSize(){
+        var newWindowWidth = $(window).width();
+        var newWindowHeight = $(window).height();
+        if (newWindowHeight < 700) {
+            $("html, body").animate({ 
+                scrollTop: $('.input-wrap').offset().top 
+            }, 1000);
+        }
+    }
+
+});
