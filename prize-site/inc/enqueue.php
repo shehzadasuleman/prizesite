e8c723fed94c7aecddadc39fdad7925a66ab7a4a
rohaanali <rohaan.ali15@gmail.com>
2019-06-14 15:19:52 +0500

    Applied fixes raised by shehzada on 20190609 ( Added missing past winners page script )

--- prize-site/inc/enqueue.php
+++ prize-site/inc/enqueue.php
@@ -59,7 +59,7 @@ function prizesite_load_scripts() {
     wp_enqueue_script( 'jquery-countdown', get_template_directory_uri() . '/js/jquery.countdown.js', array('jquery'), '2.0.2', true );
     // New Design - End
     wp_enqueue_script( 'prizesite-js', get_template_directory_uri() . '/js/prizesite.js', array('jquery'), '1.0.0', 'all' );
-    wp_enqueue_script( 'prizesite-v1-js', get_template_directory_uri() . '/js/v1/prizesite-v1.js', array('jquery'), '1.0.0', 'all' );
+    wp_enqueue_script( 'prizesite-v1-js', get_template_directory_uri() . '/js/v1/prizesite.js', array('jquery'), '1.0.0', 'all' );
     wp_enqueue_script( 'faqs-js', get_template_directory_uri() . '/js/v1/faqs.js', array('jquery'), '1.0.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'prizesite_load_scripts' );
