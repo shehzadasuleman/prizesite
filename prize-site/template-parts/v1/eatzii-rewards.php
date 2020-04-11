9a984cb60944356b37240b34868b3460fc632c80
rohaanali <rohaan.ali15@gmail.com>
2020-04-11 13:04:33 +0500

    Resolved issue of header, updated text and images on Eatzii rewards page

--- prize-site/template-parts/v1/eatzii-rewards.php
+++ prize-site/template-parts/v1/eatzii-rewards.php
@@ -7,16 +7,17 @@ $url_components = parse_url($current_uri);
 parse_str($url_components['query'], $params); 
 $customercode = $params['customercode'];
 $eatzii_logo_url = get_template_directory_uri() . "/img/eatzii/Eatzii-Logo-thumbnail.jpg";
-$eatzii_reward1_url = get_template_directory_uri() . "/img/eatzii/reward1.jpg";
-$eatzii_reward2_url = get_template_directory_uri() . "/img/eatzii/reward2.jpg";
-$eatzii_reward3_url = get_template_directory_uri() . "/img/eatzii/reward3.jpg";
-$eatzii_reward4_url = get_template_directory_uri() . "/img/eatzii/reward4.jpg";
-$eatzii_reward5_url = get_template_directory_uri() . "/img/eatzii/reward5.jpg";
-$eatzii_reward6_url = get_template_directory_uri() . "/img/eatzii/reward6.jpg";
-$eatzii_reward7_url = get_template_directory_uri() . "/img/eatzii/reward7.jpg";
-$eatzii_reward8_url = get_template_directory_uri() . "/img/eatzii/reward8.jpg";
-$eatzii_reward9_url = get_template_directory_uri() . "/img/eatzii/reward9.jpg";
-$eatzii_reward10_url = get_template_directory_uri() . "/img/eatzii/reward10.jpg";
+$eatzii_reward0_url = get_template_directory_uri() . "/img/eatzii/Reset.jpg";
+$eatzii_reward1_url = get_template_directory_uri() . "/img/eatzii/first-order.jpg";
+$eatzii_reward2_url = get_template_directory_uri() . "/img/eatzii/second-order.jpg";
+$eatzii_reward3_url = get_template_directory_uri() . "/img/eatzii/third-order.jpg";
+$eatzii_reward4_url = get_template_directory_uri() . "/img/eatzii/fourth-order.jpg";
+$eatzii_reward5_url = get_template_directory_uri() . "/img/eatzii/fifth-order.jpg";
+$eatzii_reward6_url = get_template_directory_uri() . "/img/eatzii/sixth-order.jpg";
+$eatzii_reward7_url = get_template_directory_uri() . "/img/eatzii/seventh-order.jpg";
+$eatzii_reward8_url = get_template_directory_uri() . "/img/eatzii/eight-order.jpg";
+$eatzii_reward9_url = get_template_directory_uri() . "/img/eatzii/nineth-order.jpg";
+$eatzii_reward10_url = get_template_directory_uri() . "/img/eatzii/tenth-order.jpg";
 ?>
 <img src="<?php echo $eatzii_logo_url ?>" alt="">
 <div id="eatzi-rewards-section">
@@ -38,35 +39,39 @@ $eatzii_reward10_url = get_template_directory_uri() . "/img/eatzii/reward10.jpg"
                     $orders = get_post_meta($title_exists[0]->post_id,'_etzireward_orders_value_key',true);
             ?>
                     <h1 class="font-weight-semibold">Hi <?php echo $customer_name; ?>..</h1>
-                    <?php if ( $orders % 10 === 0 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Congratulation! You have won a free Meal on us!</h6>
+                    <?php if ( $orders == 0 ) { ?>
+                        <h6 class="font-weight-normal text-muted pb-3">Order away and get free burgers on 5th and 10th order.</h6>
+                        <img src="<?php echo $eatzii_reward0_url ?>" alt="" class="img-fluid">
+                    <?php } elseif ( $orders % 10 === 0 ) { ?>
+                        <h6 class="font-weight-normal text-muted pb-3">We hope you enjoyed your Free Zinger Burger :) Now your card will be reset on the next order you will get another free Street burger on just 5 more orders. Enjoy!</h6>
                         <img src="<?php echo $eatzii_reward10_url ?>" alt="" class="img-fluid">
+                        <?php update_post_meta( $title_exists[0]->post_id, '_etzireward_orders_value_key', 0 ); ?>
                     <?php } elseif ( $orders % 10 === 1 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 4 more orders for a free Burger on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Great to see you getting started on your loyalty card. You are just 4 orders away from free Street Burger.</h6>
                         <img src="<?php echo $eatzii_reward1_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 2 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 3 more orders for a free Burger on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Thank you for your custom! You are just 3 orders away from free Street Burger.</h6>
                         <img src="<?php echo $eatzii_reward2_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 3 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 2 more orders for a free Burger on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Amazing! You are now just 2 orders away from free Street Burger.</h6>
                         <img src="<?php echo $eatzii_reward3_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 4 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 1 more orders for a free Burger on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Almost there! One your next order get a Free Street Burder on us :)</h6>
                         <img src="<?php echo $eatzii_reward4_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 5 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Congratulation! You have won a free Burger on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">We hope you enjoyed your Free Street Burger :) Now you are on your way to get Free Zinger after just 5 more orders.</h6>
                         <img src="<?php echo $eatzii_reward5_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 6 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 4 more orders for a free Meal on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">We appreciate your loyalty and custom! Thank you! Just 4 more orders for your free Zinger.</h6>
                         <img src="<?php echo $eatzii_reward6_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 7 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 3 more orders for a free Meal on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Amazing! You are now just 3 orders away from free Zinger Burger.</h6>
                         <img src="<?php echo $eatzii_reward7_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 8 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 2 more orders for a free Meal on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Thank you for your custom! You are just 2 orders away from free Zinger.</h6>
                         <img src="<?php echo $eatzii_reward8_url ?>" alt="" class="img-fluid">
                     <?php } elseif ( $orders % 10 === 9 ) { ?>
-                        <h6 class="font-weight-normal text-muted pb-3">Just make 1 more orders for a free Meal on us!</h6>
+                        <h6 class="font-weight-normal text-muted pb-3">Almost there! One your next order get a Free Zinger on us :)</h6>
                         <img src="<?php echo $eatzii_reward9_url ?>" alt="" class="img-fluid">
                     <?php } ?>
             <?php } else { ?>
